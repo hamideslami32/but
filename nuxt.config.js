f62de1e1c0621e77a2cab977047aa588aac85445
hamid <heslami32@gmail.com>
2022-05-07 19:22:35 +0430

    update: nuxt head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
 	ssr: false,
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
-		title: "portfolio",
+		title: "Vali Mohebbi Portfolio",
 		meta: [
 			{ charset: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
