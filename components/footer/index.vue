4bcac5521cedc771da6b5a6fdf6e7189c04afbba
hamid <heslami32@gmail.com>
2022-06-16 18:53:03 +0430

    fix: some style- padding margin

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -56,6 +56,7 @@ export default {
   height: 530px;
   background-color: #fff;
   // padding: 90px 140px 0 140px;
+		margin-top: 64px;
   display: flex;
   flex-direction: column;
   &__title {
--- components/share/title/index.vue
+++ components/share/title/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="title">
-    <div class="title__content container m-auto">
-      <a class="title__content__btn" href="/">
-        <i class="ri-arrow-left-line"></i>
-        {{ goBack }}
-      </a>
-      <span class="title__content__text">
-        {{ description }}
-      </span>
-    </div>
-    <div class="title__page-title">
-      <span class="title__page-title__text">
-        {{ pageTitle }}
-      </span>
-    </div>
-  </div>
+	<div class="title">
+		<div class="title__content container m-auto">
+			<a class="title__content__btn" href="/">
+				<i class="ri-arrow-left-line mr-1"></i>
+				{{ goBack }}
+			</a>
+			<span class="title__content__text">
+				{{ description }}
+			</span>
+		</div>
+		<div class="title__page-title">
+			<span class="title__page-title__text">
+				{{ pageTitle }}
+			</span>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -40,50 +40,56 @@ export default {
 
 <style lang="scss" scoped>
 .title {
-    margin: 90px 0 0 0;
-  &__content {
-    display: flex;
-    flex-direction: column;
-    // margin: 90px 140px 0 140px;
-    &__btn {
-      font-weight: 200;
-      display: flex;
-      align-items: center;
-    }
-    &__text {
-      font-size: 40px;
-      font-weight: 500;
-      margin: 20px 0 0 0;
-    }
-  }
-  &__page-title {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin: 80px 0 90px 0;
-    font-size: 60px;
-    font-weight: 500;
-  }
+	margin: 64px 20px 0;
+	@media screen and (max-width: 768px) {
+		margin: 32px 20px 0;
+	}
+	&__content {
+		display: flex;
+		flex-direction: column;
+		// margin: 90px 140px 0 140px;
+		&__btn {
+			font-weight: 200;
+			display: flex;
+			align-items: center;
+			i {
+				font-weight: bold;
+				font-size: 24px;
+			}
+		}
+		&__text {
+			font-size: 40px;
+			font-weight: 500;
+			margin-top: 20px;
+		}
+	}
+	&__page-title {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		margin: 80px 0 90px 0;
+		font-size: 60px;
+		font-weight: 500;
+	}
 }
 @media only screen and (max-width: 960px) {
-  .title {
-      margin: 30px 0 0 0;
-    &__content {
-      display: flex;
-      flex-direction: column;
-      margin: 130px 16px 0 16px;
-    }
-    &__page-title {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      font-size: 40px;
-      font-weight: 500;
-    }
-  }
+	.title {
+		margin-top: 30px;
+		&__content {
+			display: flex;
+			flex-direction: column;
+		}
+		&__page-title {
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			font-size: 40px;
+			font-weight: 500;
+		}
+	}
 }
 </style>
